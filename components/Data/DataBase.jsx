5e33d36af3d316d99e6aa2cb5fea0a043b804a0f
Manmohan Mandhan <mandhanmanmohan63@gmail.com>
2024-12-27 20:12:18 +0530

    Static Pages Setup and Tailwind Configurations

---
+++ components/Data/DataBase.jsx
@@ -0,0 +1,49 @@
+export const PageLinks = [
+    {
+        name: 'About Organization',
+        link: {
+            'Reports': '/about/reports',
+            'Governance': '/about/governance',
+            'Sponsors': '/about/sponsors',
+        }
+    },
+    {
+        name: 'Project',
+        link: {
+            'Training of PRI Members': '/project/training-of-pri-members',
+            'Old Age Home': '/project/old-age-home',
+            'Women Empowerment': '/project/women-empowerment',
+            'Child Development': '/project/child-development',
+            'Agriculture': '/project/agriculture',
+            'Health': '/project/health',
+            'Sanitization': '/project/sanitization',
+        }
+    },
+    {
+        name: 'News and Events',
+        link: {
+            'Photos': '/photos',
+            'Videos': '/videos',
+            'Events and Paper Clips': '/events-and-paper-clips',
+        }
+    },
+];
+export const data = [
+    {
+
+    },
+];
+
+export const card_data = [
+    {
+        name: 'Training of PRI Members',
+        link: '/project/training-of-pri-members',
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis inventore a earum ratione mollitia, eveniet nemo qui aliquam aliquid quos beatae nam soluta, sed atque. Necessitatibus soluta corporis temporibus perspiciatis!"
+    },
+    {
+        name: 'Old Age Home',
+        link: '/project/old-age-home',
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis inventore a earum ratione mollitia, eveniet nemo qui aliquam aliquid quos beatae nam soluta, sed atque. Necessitatibus soluta corporis temporibus perspiciatis!"
+    },
+];
+
